d06c2b5a9b6c4d607c6a04a95228f62a33a1e2ac
ipulotu <ipulotu@byui.edu>
2023-05-09 11:32:36 -0600

    Adding more images

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProjectPageComponent } from './components/project-page/project-page.component';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
 import { AboutMePageComponent } from './components/about-me-page/about-me-page.component';
+import { ResumePageComponent } from './components/resume-page/resume-page.component';
 
 
 
@@ -20,13 +24,21 @@ import { AboutMePageComponent } from './components/about-me-page/about-me-page.c
     HomePageComponent,
     ProjectPageComponent,
     ProjectCardComponent,
-    AboutMePageComponent
+    AboutMePageComponent,
+    ResumePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot({
+      timeOut: 1000000,
+      preventDuplicates: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
